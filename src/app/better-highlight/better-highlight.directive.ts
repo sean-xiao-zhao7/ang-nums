3a50a28037a2bd9ca7da96b051fa6fb5661572e2
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-11-18 16:26:48 -0500

    Add appUnless structural directive

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,7 +1,9 @@
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2,
 } from '@angular/core';
@@ -10,23 +12,32 @@ import {
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+
+  @HostBinding('style.backgroundColor') backgroundColorValue: string;
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.backgroundColorValue = this.defaultColor;
+  }
 
   @HostListener('mouseenter') mouseover(eventData) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'yellow'
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'yellow'
+    // );
+    this.backgroundColorValue = this.highlightColor;
   }
 
   @HostListener('mouseleave') mousegone(eventData) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
+    this.backgroundColorValue = this.defaultColor;
   }
 }
